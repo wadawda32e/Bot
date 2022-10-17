85cbaaa96ad30eccc4ec8d9c87b4c6a7eae62049
wadawda32e <115761715+wadawda32e@users.noreply.github.com>
2022-10-17 08:31:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Subscribe To Easy Gamer : https://www.youtube.com/channel/UCk575VDlrCsv1Gy2XZQ1bSA")
+	bot.chat("لاحد ينسى القوانين")
 	first = false;
 	}
 	else{
-		bot.chat("Subscribe To MannuG : https://www.youtube.com/channel/UCSZHRnBue50ImBKeR6k6eDg")
+		bot.chat("لاحد ينسى القوانين")
 		first = true;
 	}
   }, 3600000 * i); 
